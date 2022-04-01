0c3769a5c1122e8b6cfbebe5727e538a4abf70ba
Mpablom <pablomoya211@gmail.com>
2022-03-31 22:15:21 -0300

    pase el portfolio de html y css a (angular)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aprendiendo_angular';
+  nombre:string= "Pablo Moya"
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { DatosPersonalesComponent } from './componentes/datos-personales/datos-personales.component';
+import { FormacionAcademicaComponent } from './componentes/formacion-academica/formacion-academica.component';
+import { ExperienciaLaboralComponent } from './componentes/experiencia-laboral/experiencia-laboral.component';
+import { DatosInteresComponent } from './componentes/datos-interes/datos-interes.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    DatosPersonalesComponent,
+    FormacionAcademicaComponent,
+    ExperienciaLaboralComponent,
+    DatosInteresComponent
   ],
   imports: [
     BrowserModule,
